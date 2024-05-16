f3510a8cfb817858f0a8ce620a29d0d884af0d9c
Arian-Ghaemi <arianghaemi@gmail.com>
2024-05-16 11:05:20 +0200

    first commit

---
+++ streamlit_task_1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("Streamlit Task")
+
+st.subheader("steamlit Subtask 1")
+
+st.write("Greetings world")
+st.write("My name is Rick")
+
+st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
